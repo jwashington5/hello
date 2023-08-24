7cfa0492fc7e379a319bb1d68fe3232e3ce47e71
jwashington5 <jakobiwashington1@gmail.com>
2023-08-24 11:27:14 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jakobi Washington
+ * Date: 2023/08/24
+ *
+ * This program
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Jakobi Washington\n");
+
+    return 0;
+}
